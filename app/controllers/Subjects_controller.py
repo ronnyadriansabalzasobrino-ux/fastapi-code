578092e544ae6f987038c418f8b90c40a2ab3396
ronnyadriansabalzasobrino <ronnyadriansabalzasobrino@gmail.com>
2026-05-11 14:12:29 -0500

    fix subjects program

--- app/controllers/Subjects_controller.py
+++ app/controllers/Subjects_controller.py
@@ -71,7 +71,18 @@ class SubjectsController:
             conn = get_db_connection()
             cursor = conn.cursor()
 
-            cursor.execute("SELECT * FROM Subject")
+            cursor.execute("""
+                SELECT
+                    s.id_subject,
+                    s.name_subject,
+                    s.credits,
+                    s.id_program,
+                    p.name
+                FROM Subject s
+                LEFT JOIN Program p
+                ON s.id_program = p.id_program
+            """)
+
             result = cursor.fetchall()
 
             payload = []
@@ -81,7 +92,8 @@ class SubjectsController:
                     "id_subject": row[0],
                     "name_subject": row[1],
                     "credits": row[2],
-                    "id_program": row[3]
+                    "id_program": row[3],
+                    "program": row[4]
                 })
 
             return jsonable_encoder(payload)
@@ -92,7 +104,6 @@ class SubjectsController:
 
         finally:
             conn.close()
-
 
     def update_subject(self, id_subject: int, subject: Subjects):
         try:
