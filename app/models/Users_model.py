fe92c6372a4b214e5a623d7bdaaeb9d8e4a5e267
ronnyadriansabalzasobrino <ronnyadriansabalzasobrino@gmail.com>
2026-05-12 13:26:00 -0500

    registro

--- app/models/Users_model.py
+++ app/models/Users_model.py
@@ -1,11 +1,11 @@
 from pydantic import BaseModel
-
+from typing import Optional
 class Users(BaseModel):
     id_user: int | None = None
     name: str
     last_name: str
-    post: str
+    post: Optional[str] = ""
     mail: str
-    phone: str
+    phone: Optional[str] = ""
     rol: str
     password: str
