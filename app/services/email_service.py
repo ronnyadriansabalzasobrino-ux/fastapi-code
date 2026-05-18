6e1478968d807a20bdab1bc5006df0ef1aa02a2d
ronnyadriansabalzasobrino <ronnyadriansabalzasobrino@gmail.com>
2026-05-18 14:47:28 -0500

    cambios email

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -1,29 +1,54 @@
-from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+import smtplib
 
-conf = ConnectionConfig(
-    MAIL_USERNAME="ronnyadriansabalzasobrino@gmail.com",
-    MAIL_PASSWORD="lwjk jylo rhsw jzqo",
-    MAIL_FROM="ronnyadriansabalzasobrino@gmail.com",
-    MAIL_PORT=587,
-    MAIL_SERVER="smtp.gmail.com",
-    MAIL_STARTTLS=True,
-    MAIL_SSL_TLS=False,
-    USE_CREDENTIALS=True,
-    VALIDATE_CERTS=True
-)
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+
+EMAIL = "ronnyadriansabalzasobrino@gmail.com"
+PASSWORD = "lwjk jylo rhsw jzqo"
 
 ADMIN_EMAIL = "ronnyadriansabalzasobrino@gmail.com"
 
 
 async def send_email(destinatario: str, asunto: str, contenido: str):
 
-    message = MessageSchema(
-        subject=asunto,
-        recipients=[destinatario],
-        body=contenido,
-        subtype="html"
-    )
+    try:
 
-    fm = FastMail(conf)
+        mensaje = MIMEMultipart()
 
-    await fm.send_message(message)
+        mensaje["From"] = EMAIL
+        mensaje["To"] = destinatario
+        mensaje["Subject"] = asunto
+
+        mensaje.attach(
+            MIMEText(contenido, "html")
+        )
+
+        servidor = smtplib.SMTP(
+            SMTP_SERVER,
+            SMTP_PORT
+        )
+
+        servidor.starttls()
+
+        servidor.login(
+            EMAIL,
+            PASSWORD
+        )
+
+        servidor.sendmail(
+            EMAIL,
+            destinatario,
+            mensaje.as_string()
+        )
+
+        servidor.quit()
+
+        print("Correo enviado correctamente")
+
+    except Exception as e:
+
+        print("ERROR CORREO:", e)
