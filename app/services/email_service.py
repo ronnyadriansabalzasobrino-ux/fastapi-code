2cd1df3249e5b46a768ff43dd2bd2af2f183d352
ronnyadriansabalzasobrino <ronnyadriansabalzasobrino@gmail.com>
2026-05-18 14:01:34 -0500

    correo

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -1,15 +1,19 @@
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+
 conf = ConnectionConfig(
     MAIL_USERNAME="ronnyadriansabalzasobrino@gmail.com",
     MAIL_PASSWORD="lwjk jylo rhsw jzqo",
     MAIL_FROM="ronnyadriansabalzasobrino@gmail.com",
-    MAIL_PORT=587,
+    MAIL_PORT=465,
     MAIL_SERVER="smtp.gmail.com",
-    MAIL_STARTTLS=True,
-    MAIL_SSL_TLS=False,
-    USE_CREDENTIALS=True
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True
 )
-ADMIN_EMAIL="ronnyadriansabalzasobrino@gmail.com"
+
+ADMIN_EMAIL = "ronnyadriansabalzasobrino@gmail.com"
+
 
 async def send_email(destinatario: str, asunto: str, contenido: str):
 
@@ -21,4 +25,5 @@ async def send_email(destinatario: str, asunto: str, contenido: str):
     )
 
     fm = FastMail(conf)
+
     await fm.send_message(message)
